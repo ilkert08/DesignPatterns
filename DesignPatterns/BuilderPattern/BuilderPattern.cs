0b951ce080c3228ba9d59e6d9737a280c56ef024
ilkert08 <ilkertnkr@gmail.com>
2022-08-21 23:59:09 +0300

    semaphore should be replaced with a mutex lock.

---
+++ DesignPatterns/BuilderPattern/BuilderPattern.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BuilderPattern
+{
+    class BuilderPattern
+    {
+
+
+    }
+
+
+    public class SingleItem
+    {
+        private static SingleItem item = null;
+
+        private SingleItem()
+        {
+            this.Name = "ilker";
+        }
+
+        public string Name { get; set;}
+
+
+        public static SingleItem GetSingleItem()
+        {
+            if(item == null)
+            {
+                item = new SingleItem();
+            }
+            return item;
+        }
+    }
+
+
+    public class SingleItemMultiThread
+    {
+        private static SingleItemMultiThread item = null;
+
+        private SingleItemMultiThread()
+        {
+            this.Name = "ilker";
+        }
+
+        public string Name { get; set; }
+
+
+        public static SingleItemMultiThread GetSingleItem()
+        {
+            SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+            semaphore.WaitAsync();
+            if (item == null)
+            {
+                item = new SingleItemMultiThread();
+            }
+            semaphore.Release();
+            return item;
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using DesignPatterns.BuilderPattern;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // singleThreadSingleton();
+            multiThreadSingleton();
+
+        }
+
+
+        private static void singleThreadSingleton()
+        {
+            SingleItem item = SingleItem.GetSingleItem();
+            Console.WriteLine("Item name: {0}", item.Name);
+
+            item.Name = "Ali";
+            Console.WriteLine("Item name: {0}", item.Name);
+
+            SingleItem item2 = SingleItem.GetSingleItem();
+            Console.WriteLine("Item name: {0}", item.Name);
+
+            item2.Name = "Kübra";
+            Console.WriteLine("Item name: {0}", item.Name);
+        }
+
+        private static void multiThreadSingleton()
+        {
+            var item = SingleItemMultiThread.GetSingleItem();
+            Console.WriteLine("Item name: {0}", item.Name);
+
+            item.Name = "Ali";
+            Console.WriteLine("Item name: {0}", item.Name);
+
+            var item2 = SingleItemMultiThread.GetSingleItem();
+            Console.WriteLine("Item name: {0}", item.Name);
+
+            item2.Name = "Kübra";
+            Console.WriteLine("Item name: {0}", item.Name);
+        }
+    }
+}
